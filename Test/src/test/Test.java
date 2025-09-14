42aec15ba747d8554a1bb6ea757c6db49d38fe0e
JosephAB1 <jefry_2001_00@outlook.com>
2025-09-14 18:34:05 -0500

    Agregando cambios Recientes

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package actividad.github;
+package test;
 
 /**
  *
  * @author joseph
  */
-public class ActividadGitHub {
+public class Test {
 
     /**
      * @param args the command line arguments
